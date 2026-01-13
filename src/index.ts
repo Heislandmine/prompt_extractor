fa0c6a5550b247ac7565451ef47825ea50487f18
heislandmine <heislandmine@protonmail.com>
2026-01-13 10:47:23 +0900

    バイト列成形表示用の関数bytesToSpacedHexStringを実装

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,20 @@ const extractPngSig = (path: string) => {
   return buffer.subarray(0, 8);
 };
 
+/**
+ * バイト列を1バイトずつスペースで区切られた文字列に変換する。
+ * @param buffer 変換元のバイト列
+ * @returns
+ */
+const bytesToSpacedHexString = (buffer: Buffer<ArrayBuffer>): string => {
+  const arr = Array.from(buffer); // Buffer<ArrayBuffer>のままだと.mapで文字列の配列を作れないのでArrayに変換する。
+
+  return arr.map((e) => e.toString(16).padStart(2, '0').toUpperCase()).join(' ');
+};
+
 const arg = process.argv[2];
 
 if (arg) {
-  console.log(extractPngSig(arg).toString('hex'));
+  const result = extractPngSig(arg);
+  console.log(bytesToSpacedHexString(result));
 }
