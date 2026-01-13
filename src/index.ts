980a93e5fc17b9bd2907355c14cbca0dc2ac6348
heislandmine <heislandmine@protonmail.com>
2026-01-13 10:10:23 +0900

    pngのシグネチャ抽出関数の初版を実装

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import { readFileSync } from 'fs';
+
 const extractPngSig = (path: string) => {
-  console.log(path);
+  const buffer = readFileSync(path);
+
+  return buffer.subarray(0, 8);
 };
 
 const arg = process.argv[2];
 
 if (arg) {
-  extractPngSig(arg);
+  console.log(extractPngSig(arg).toString('hex'));
 }
